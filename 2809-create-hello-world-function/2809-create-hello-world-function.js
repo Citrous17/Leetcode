71560af400d82af636a02d0ed229aa02366f500e
Kyle Stallings <citrous@tamu.edu>
2023-08-24 12:04:20 -0500

    Time: 65 ms (6.95%), Space: 41.7 MB (54.78%) - LeetHub

---
+++ 2809-create-hello-world-function/2809-create-hello-world-function.js
@@ -0,0 +1,16 @@
+/**
+ * @return {Function}
+ */
+ //Kyle Stallings
+ // Aug 24 2023
+ //Not really sure what the challenge is here...
+var createHelloWorld = function() {
+    return function(...args) {
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
